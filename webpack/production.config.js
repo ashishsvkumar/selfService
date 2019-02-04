9b5b2bd1e92531fd3d69eb295551126498041222
Anurag Saini <anurag.saini@redmart.com>
2019-02-04 15:22:53 +0530

    Enable compression for prod

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,9 +1,21 @@
 const merge = require('webpack-merge');
 const commonConfig = require('./common.config.js');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const zopfli = require('@gfx/zopfli');
 
 module.exports = merge(commonConfig, {
     optimization: {
         minimizer: [new UglifyJsPlugin()],
-    }
+    },
+    plugins: [
+        new CompressionPlugin({
+            compressionOptions: {
+                numiterations: 15
+            },
+            algorithm(input, compressionOptions, callback) {
+                return zopfli.gzip(input, compressionOptions, callback);
+            }
+        })
+    ]
 });
