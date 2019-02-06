27b2c4b895322096e1fe00e173dfd96851855712
Anurag Saini <anurag.saini@redmart.com>
2019-02-06 13:12:32 +0530

    Desktop changes

--- src/components/card/ContactUs.tsx
+++ src/components/card/ContactUs.tsx
@@ -14,18 +14,16 @@ export const ContactUs = (props: ContactUsProps) => {
             </div>
             <div className={styles.only_desktop}>
                 <div className={cx([styles.only_desktop, styles.title])}>Need further help?</div>
+                <div className={styles.subtitle}>Our customer support team will be here to assist you.</div>
                 <div className={styles.btns}>
                     <div className={styles.btn} onClick={props.onChatClick}>
                         <div className={styles.chat} />
                         <div className={styles.label}>{props.chatMessage}</div>
                     </div>
-                    <a className={styles.btn} href="tel:+6562613456">
-                        <div className={styles.call} />
-                        <div className={styles.label}>
-                            <span className={styles.only_mobile}>Call us</span>
-                            <span className={styles.only_desktop}>Call +65 6261 3456</span>
-                        </div>
-                    </a>
+                    <div className={cx([styles.btn, styles.disabled])} onClick={props.onChatClick}>
+                        <div className={styles.chat} />
+                        <div className={styles.label}>More Options</div>
+                    </div>
                 </div>
                 <div className={styles.subtext}>7am - 11pm SGT Daily</div>
             </div>
