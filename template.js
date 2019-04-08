bbff8c5b68fa4ceb61d59ff6c3eb4b2856eb56d8
Anurag Saini <anurag.saini@redmart.com>
2019-04-08 14:12:16 +0530

    Enable GA for SSv3

--- template.js
+++ template.js
@@ -65,6 +65,16 @@ function prepareTemplate(head, header, footer, isMobile) {
         <script>__rm__env__="${process.env.NODE_ENV}"</script>
         <script>__rm__device__="${isMobile ? 'mobile' : 'desktop'}"</script>
         ${head}
+        <!-- Google Analytics -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-26494190-22', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <!-- End Google Analytics -->
     </head>
     <body>
         ${header}
