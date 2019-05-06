c9ba6abf9880b93bc396c42986ff1fed629e56f2
Anurag Saini <anurag.saini@redmart.com>
2019-05-06 14:59:47 +0530

    Add field ‘area-label’

--- src/components/labels/ContentTitle.tsx
+++ src/components/labels/ContentTitle.tsx
@@ -5,4 +5,4 @@ export interface TitleProps {
   text: string;
 }
 
-export const ContentTitle = (props: TitleProps) => <h3 className={styles.content}>{props.text}</h3>;
+export const ContentTitle = (props: TitleProps) => <h3 className={styles.content} area-label={props.text.toUpperCase()}>{props.text}</h3>;
