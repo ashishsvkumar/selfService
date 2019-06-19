077a9ada83d552a58525193c1532ea487a12596a
Anurag Saini <anurag.saini@redmart.com>
2019-06-19 12:10:12 +0800

    Point to K8

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -2,7 +2,8 @@ import { currentEnvironment, Environments } from "./environment";
 
 function getSelfServiceBase() {
   if (currentEnvironment === Environments.preLive) {
-    return "//selfservice-api.alpha.redmart.com/v1.0.0/support";;
+    //return "//selfservice-api.alpha.redmart.com/v1.0.0/support";
+    return "//support-service.ecommerce-api.alpha.redmart.com/v1.0.0/v1.0/support";
   }
   return "//selfservice-api.redmart.com/v1.0.0/support";
 }
